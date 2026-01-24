d78b99a9d9e72dd07315ce69bdeabf7479265f5c
osoohynn <tbr335@naver.com>
2026-01-24 13:30:28 +0900

    initial commit

---
+++ src/main/java/com/dnd5/timoapi/TimoBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.dnd5.timoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimoBackendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimoBackendApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dnd5/timoapi/TimoBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd5.timoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimoBackendApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
