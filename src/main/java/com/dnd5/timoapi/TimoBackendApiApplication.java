a4e5b4f06d7ccc670c933f1131732d22237c72ae
osoohynn <tbr335@naver.com>
2026-01-26 14:12:04 +0900

    feat: Jpa 설정

--- src/main/java/com/dnd5/timoapi/TimoBackendApiApplication.java
+++ src/main/java/com/dnd5/timoapi/TimoBackendApiApplication.java
@@ -2,7 +2,9 @@ package com.dnd5.timoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TimoBackendApiApplication {
 
