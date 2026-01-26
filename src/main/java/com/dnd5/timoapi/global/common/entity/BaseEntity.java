e891fb9b6a69aa3fd15843d01fd700e0af887e21
osoohynn <tbr335@naver.com>
2026-01-26 14:00:41 +0900

    feat: createdAt, updatedAt에 어노테이션 추가

--- src/main/java/com/dnd5/timoapi/global/common/entity/BaseEntity.java
+++ src/main/java/com/dnd5/timoapi/global/common/entity/BaseEntity.java
@@ -1,6 +1,9 @@
 package com.dnd5.timoapi.global.common.entity;
 
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import java.time.LocalDateTime;
 
 @MappedSuperclass
@@ -10,9 +13,11 @@ public abstract class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
