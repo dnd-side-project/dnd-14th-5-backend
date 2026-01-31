c5d711a66d4f11402d63d224eab90f0743db92cb
osoohynn <tbr335@naver.com>
2026-01-31 19:35:00 +0900

    feat: 테스트 환경 구성 (H2 인메모리 DB)

--- src/test/java/com/dnd5/timoapi/TimoBackendApiApplicationTests.java
+++ src/test/java/com/dnd5/timoapi/TimoBackendApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.dnd5.timoapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TimoBackendApiApplicationTests {
 
     @Test
