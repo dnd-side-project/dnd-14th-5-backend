45695a30e61be528adc05193d47071b2ab2835f2
reyang <didfodms@gmail.com>
2026-02-02 22:52:30 +0900

    fix: 불필요한 메소드 삭제

--- src/main/java/com/dnd5/timoapi/domain/test/presentation/request/UserTestResponseCreateRequest.java
+++ src/main/java/com/dnd5/timoapi/domain/test/presentation/request/UserTestResponseCreateRequest.java
@@ -1,6 +1,5 @@
 package com.dnd5.timoapi.domain.test.presentation.request;
 
-import com.dnd5.timoapi.domain.test.domain.model.UserTestResponse;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
@@ -10,7 +9,4 @@ public record UserTestResponseCreateRequest(
         @Positive
         int score
 ) {
-    public UserTestResponse toModel(@Positive @NotNull Long testRecordId) {
-        return UserTestResponse.create(testRecordId, questionId, score);
-    }
 }
