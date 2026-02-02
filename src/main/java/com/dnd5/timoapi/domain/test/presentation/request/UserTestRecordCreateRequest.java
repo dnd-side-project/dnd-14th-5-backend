a59afa140d12c170fdd797127f408ebbdfccd753
reyang <didfodms@gmail.com>
2026-02-02 20:09:01 +0900

    fix: @Positive 어노테이션 추가

--- src/main/java/com/dnd5/timoapi/domain/test/presentation/request/UserTestRecordCreateRequest.java
+++ src/main/java/com/dnd5/timoapi/domain/test/presentation/request/UserTestRecordCreateRequest.java
@@ -2,9 +2,10 @@ package com.dnd5.timoapi.domain.test.presentation.request;
 
 import com.dnd5.timoapi.domain.test.domain.model.UserTestRecord;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record UserTestRecordCreateRequest(
-        @NotNull
+        @Positive @NotNull
         Long testId
 ) {
     public UserTestRecord toModel() {
