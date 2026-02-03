412001632dfbc0639adfc61e2736817231072deb
reyang <didfodms@gmail.com>
2026-02-03 19:27:41 +0900

    feat: GET 피드백 프롬프트 전체 조회 기능 추가
    - controller에 GET /feedback-prompts 생성
    - service에 findAll 로직 생성
    - response DTO 생성

--- src/main/java/com/dnd5/timoapi/domain/reflection/application/service/ReflectionFeedbackPromptService.java
+++ src/main/java/com/dnd5/timoapi/domain/reflection/application/service/ReflectionFeedbackPromptService.java
@@ -4,6 +4,10 @@ import com.dnd5.timoapi.domain.reflection.domain.entity.ReflectionFeedbackPrompt
 import com.dnd5.timoapi.domain.reflection.domain.model.ReflectionFeedbackPrompt;
 import com.dnd5.timoapi.domain.reflection.domain.repository.ReflectionFeedbackPromptRepository;
 import com.dnd5.timoapi.domain.reflection.presentation.request.ReflectionFeedbackPromptCreateRequest;
+import com.dnd5.timoapi.domain.reflection.presentation.response.ReflectionFeedbackPromptResponse;
+import com.dnd5.timoapi.domain.test.domain.entity.TestEntity;
+import com.dnd5.timoapi.domain.test.presentation.response.TestResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,5 +24,13 @@ public class ReflectionFeedbackPromptService {
     public void create(ReflectionFeedbackPromptCreateRequest request) {
         ReflectionFeedbackPrompt ReflectionFeedbackPrompt = request.toModel();
         reflectionFeedbackPromptRepository.save(ReflectionFeedbackPromptEntity.from(ReflectionFeedbackPrompt));
+    }
+
+    @Transactional(readOnly = true)
+    public List<ReflectionFeedbackPromptResponse> findAll() {
+        return reflectionFeedbackPromptRepository.findAll().stream()
+                .map(ReflectionFeedbackPromptEntity::toModel)
+                .map(ReflectionFeedbackPromptResponse::from)
+                .toList();
     }
 }
--- src/main/java/com/dnd5/timoapi/domain/reflection/presentation/AdminReflectionFeedbackPromptController.java
+++ src/main/java/com/dnd5/timoapi/domain/reflection/presentation/AdminReflectionFeedbackPromptController.java
@@ -3,12 +3,16 @@ package com.dnd5.timoapi.domain.reflection.presentation;
 import com.dnd5.timoapi.domain.reflection.application.service.ReflectionFeedbackPromptService;
 import com.dnd5.timoapi.domain.reflection.domain.model.ReflectionFeedbackPrompt;
 import com.dnd5.timoapi.domain.reflection.presentation.request.ReflectionFeedbackPromptCreateRequest;
+import com.dnd5.timoapi.domain.reflection.presentation.response.ReflectionFeedbackPromptResponse;
 import com.dnd5.timoapi.domain.test.domain.entity.TestEntity;
 import com.dnd5.timoapi.domain.test.presentation.request.TestCreateRequest;
+import com.dnd5.timoapi.domain.test.presentation.response.TestResponse;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +31,11 @@ public class AdminReflectionFeedbackPromptController {
     @ResponseStatus(HttpStatus.CREATED)
     public void create(@Valid @RequestBody ReflectionFeedbackPromptCreateRequest request) {
         reflectionFeedbackPromptService.create(request);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ReflectionFeedbackPromptResponse> findAll() {
+        return reflectionFeedbackPromptService.findAll();
     }
 }
