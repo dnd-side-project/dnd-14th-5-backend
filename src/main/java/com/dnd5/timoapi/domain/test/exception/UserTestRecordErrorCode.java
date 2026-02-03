1d5b08fa8ad78b2307e30fe3e8044caec35c5781
reyang <didfodms@gmail.com>
2026-02-03 22:30:21 +0900

    fix: 에러 상태 코드 변경

--- src/main/java/com/dnd5/timoapi/domain/test/exception/UserTestRecordErrorCode.java
+++ src/main/java/com/dnd5/timoapi/domain/test/exception/UserTestRecordErrorCode.java
@@ -10,8 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum UserTestRecordErrorCode implements ErrorCode {
 
     USER_TEST_RECORD_NOT_FOUND(HttpStatus.NOT_FOUND, "유저의 테스트 기록을 찾을 수 없습니다."),
-    ALREADY_COMPLETED(HttpStatus.ALREADY_REPORTED, "유저의 테스트 기록이 이미 완료되었습니다."),
-    NOT_ALL_QUESTIONS_ANSWERED(HttpStatus.EXPECTATION_FAILED, "유저가 테스트 문항에 모두 응답하지 않았습니다.")
+    ALREADY_COMPLETED(HttpStatus.CONFLICT, "유저의 테스트 기록이 이미 완료되었습니다."),
+    NOT_ALL_QUESTIONS_ANSWERED(HttpStatus.BAD_REQUEST, "유저가 테스트 문항에 모두 응답하지 않았습니다.")
     ;
 
     private final HttpStatus status;
