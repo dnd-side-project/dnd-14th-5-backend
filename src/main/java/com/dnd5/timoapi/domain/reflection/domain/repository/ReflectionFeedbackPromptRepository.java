2dea10de71b8402ceb3d8cff31e1ff2b0dfedbc7
reyang <didfodms@gmail.com>
2026-02-09 02:42:27 +0900

    feat: latest version 기준 findBy 메소드 생성
    - 피드백 생성 로직에 프롬프트 version을 전달하지 않더라도, 최신 버전의 프롬프트를 사용하도록 로직 수정

--- src/main/java/com/dnd5/timoapi/domain/reflection/domain/repository/ReflectionFeedbackPromptRepository.java
+++ src/main/java/com/dnd5/timoapi/domain/reflection/domain/repository/ReflectionFeedbackPromptRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ReflectionFeedbackPromptRepository extends
         JpaRepository<ReflectionFeedbackPromptEntity, Long> {
     Optional<ReflectionFeedbackPromptEntity> findByVersion(int version);
+    Optional<ReflectionFeedbackPromptEntity> findTopByDeletedAtIsNullOrderByVersionDesc();
 }
