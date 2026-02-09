83bf207b8196f8ea86e8f76899f333bf9e7e6011
reyang <didfodms@gmail.com>
2026-02-10 01:31:45 +0900

    refactor: getLatestPrompt 메소드 생성
    - getLatestPrompt를 통해 최신 버전의 프롬프트로 피드백 생성
    - 사용하지 않는 import 구문 삭제

--- src/main/java/com/dnd5/timoapi/domain/reflection/infrastructure/ai/FeedbackGeneratorImpl.java
+++ src/main/java/com/dnd5/timoapi/domain/reflection/infrastructure/ai/FeedbackGeneratorImpl.java
@@ -1,5 +1,6 @@
 package com.dnd5.timoapi.domain.reflection.infrastructure.ai;
 
+import com.dnd5.timoapi.domain.reflection.application.service.ReflectionFeedbackPromptService;
 import com.dnd5.timoapi.domain.reflection.domain.entity.ReflectionFeedbackPromptEntity;
 import com.dnd5.timoapi.domain.reflection.domain.repository.ReflectionFeedbackPromptRepository;
 import com.dnd5.timoapi.domain.reflection.exception.ReflectionErrorCode;
@@ -17,6 +18,8 @@ public class FeedbackGeneratorImpl implements FeedbackGenerator {
     private final GeminiClient geminiClient;
     private final ObjectMapper objectMapper;
 
+    private final ReflectionFeedbackPromptService reflectionFeedbackPromptService;
+
     private final ReflectionFeedbackPromptRepository reflectionFeedbackPromptRepository;
 
     @Override
@@ -29,9 +32,15 @@ public class FeedbackGeneratorImpl implements FeedbackGenerator {
         return parseResponse(response);
     }
 
-    // FIXME: @didfodms 프롬프트 작성 / 프롬프트도 데이터베이스에 저장하면 어떨까요?
     private String buildPrompt(ZtpiCategory category, String question, String userReflection) {
         return getLatestPrompt().formatted(category.name(), question, userReflection);
+    }
+
+    private String getLatestPrompt() {
+        ReflectionFeedbackPromptEntity reflectionFeedbackPromptEntity =  reflectionFeedbackPromptRepository.findTopByDeletedAtIsNullOrderByVersionDesc()
+                .orElseThrow(() -> new BusinessException(ReflectionErrorCode.REFLECTION_FEEDBACK_PROMPT_NOT_FOUND));
+
+        return reflectionFeedbackPromptEntity.getContent();
     }
 
     private FeedbackResult parseResponse(String response) {
@@ -41,13 +50,6 @@ public class FeedbackGeneratorImpl implements FeedbackGenerator {
         } catch (Exception e) {
             throw new RuntimeException("Failed to parse Gemini response", e);
         }
-    }
-
-    private String getLatestPrompt() {
-        ReflectionFeedbackPromptEntity reflectionFeedbackPromptEntity =  reflectionFeedbackPromptRepository.findTopByDeletedAtIsNullOrderByVersionDesc()
-                .orElseThrow(() -> new BusinessException(ReflectionErrorCode.REFLECTION_FEEDBACK_PROMPT_NOT_FOUND));
-
-        return reflectionFeedbackPromptEntity.getContent();
     }
 
 }
