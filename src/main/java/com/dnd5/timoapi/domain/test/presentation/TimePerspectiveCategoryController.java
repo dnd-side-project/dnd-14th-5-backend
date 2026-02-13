b55c582786571b7869339bdc81f418c3ea1e0764
reyang <didfodms@gmail.com>
2026-02-13 19:34:09 +0900

    feat: controller 추가
    - 환경변수 예시 .env.example kakao 항목 추가
    - 클라이언트 키, 시크릿 키 설정 가능

---
+++ src/main/java/com/dnd5/timoapi/domain/test/presentation/TimePerspectiveCategoryController.java
@@ -0,0 +1,14 @@
+package com.dnd5.timoapi.domain.test.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/time-perspective-category")
+@RequiredArgsConstructor
+@Validated
+public class TimePerspectiveCategoryController {
+
+}
