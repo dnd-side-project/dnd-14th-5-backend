acc233c2150909d8cfdbf4ceeb83a2af654cdaba
reyang <didfodms@gmail.com>
2026-02-17 05:27:20 +0900

    fix: 예외 처리 객체 additional 포함 가능하도록 수정

--- src/main/java/com/dnd5/timoapi/global/exception/BusinessException.java
+++ src/main/java/com/dnd5/timoapi/global/exception/BusinessException.java
@@ -1,15 +1,29 @@
 package com.dnd5.timoapi.global.exception;
 
+import java.util.Map;
+
 public class BusinessException extends RuntimeException {
 
     private final ErrorCode errorCode;
+    private final Map<String, Object> additional; // 선택적 추가 정보
 
     public BusinessException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
+        this.additional = null;
+    }
+
+    public BusinessException(ErrorCode errorCode, Map<String, Object> additional) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+        this.additional = additional;
     }
 
     public ErrorCode getErrorCode() {
         return errorCode;
+    }
+
+    public Map<String, Object> getAdditional() {
+        return additional;
     }
 }
