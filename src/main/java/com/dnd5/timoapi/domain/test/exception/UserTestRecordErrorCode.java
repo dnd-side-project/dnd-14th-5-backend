d2e43c6a3b49b9566ccfea8a2bea1c83fa5bc4ca
reyang <didfodms@gmail.com>
2026-02-17 05:25:20 +0900

    feat: 409 에러 코드 추가
    - 이미 진행 중인 테스트 기록이 있는 경우 예외 처리 코드 추가

--- src/main/java/com/dnd5/timoapi/domain/test/exception/UserTestRecordErrorCode.java
+++ src/main/java/com/dnd5/timoapi/domain/test/exception/UserTestRecordErrorCode.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum UserTestRecordErrorCode implements ErrorCode {
 
     USER_TEST_RECORD_NOT_FOUND(HttpStatus.NOT_FOUND, "유저의 테스트 기록을 찾을 수 없습니다."),
+    ALREADY_IN_PROGRESS(HttpStatus.CONFLICT, "유저의 테스트 기록이 이미 진행 중입니다."),
     ALREADY_COMPLETED(HttpStatus.CONFLICT, "유저의 테스트 기록이 이미 완료되었습니다."),
     NOT_ALL_QUESTIONS_ANSWERED(HttpStatus.BAD_REQUEST, "유저가 테스트 문항에 모두 응답하지 않았습니다."),
     USER_TEST_RESULT_NOT_FOUND(HttpStatus.NOT_FOUND, "테스트 기록의 결과가 존재하지 않습니다.")
