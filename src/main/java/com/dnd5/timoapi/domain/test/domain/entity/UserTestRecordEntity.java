0929e0ef46e4b51075b2597a26f9cb377c39dee1
reyang <didfodms@gmail.com>
2026-02-27 14:24:37 +0900

    feat: 사용자 검사 완료시 category 업데이트
    - 테스트 완료할 경우, category를 검사 결과의 최댓값으로 업데이트

--- src/main/java/com/dnd5/timoapi/domain/test/domain/entity/UserTestRecordEntity.java
+++ src/main/java/com/dnd5/timoapi/domain/test/domain/entity/UserTestRecordEntity.java
@@ -1,7 +1,9 @@
 package com.dnd5.timoapi.domain.test.domain.entity;
 
 import com.dnd5.timoapi.domain.test.domain.model.UserTestRecord;
+import com.dnd5.timoapi.domain.test.domain.model.UserTestResult;
 import com.dnd5.timoapi.domain.test.domain.model.enums.TestRecordStatus;
+import com.dnd5.timoapi.domain.test.domain.model.enums.ZtpiCategory;
 import com.dnd5.timoapi.domain.user.domain.entity.UserEntity;
 import com.dnd5.timoapi.global.common.entity.BaseEntity;
 import jakarta.persistence.Column;
@@ -12,6 +14,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.util.Map;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
