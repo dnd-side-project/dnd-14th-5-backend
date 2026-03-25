6d191d2a5f2e91ff65fb9591af83d7b6c73a10f3
reyang <didfodms@gmail.com>
2026-03-26 03:49:21 +0900

    fix: delete 메소드 트랜잭션 어노테이션 readOnly 제거

--- src/main/java/com/dnd5/timoapi/domain/test/application/service/TestService.java
+++ src/main/java/com/dnd5/timoapi/domain/test/application/service/TestService.java
@@ -66,7 +66,7 @@ public class TestService {
         testEntity.update(request.type(), request.name(), request.description());
     }
 
-    @Transactional(readOnly = true)
+    @Transactional
     public void delete(Long testId) {
         TestEntity testEntity = getTestEntity(testId);
 
