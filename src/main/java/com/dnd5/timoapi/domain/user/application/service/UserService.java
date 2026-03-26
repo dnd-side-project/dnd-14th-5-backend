997f6dbc73935983204081cb239dc867724d6ca6
reyang <didfodms@gmail.com>
2026-03-26 22:08:26 +0900

    refactor: 유저 삭제시, user test record와 자식 관계 엔티티까지 삭제 처리
    - 이후 분석을 위해 모두 soft delete로 구현

--- src/main/java/com/dnd5/timoapi/domain/user/application/service/UserService.java
+++ src/main/java/com/dnd5/timoapi/domain/user/application/service/UserService.java
@@ -1,6 +1,9 @@
 package com.dnd5.timoapi.domain.user.application.service;
 
 import com.dnd5.timoapi.domain.auth.domain.repository.RefreshTokenRepository;
+import com.dnd5.timoapi.domain.test.application.service.UserTestRecordService;
+import com.dnd5.timoapi.domain.test.domain.entity.UserTestRecordEntity;
+import com.dnd5.timoapi.domain.test.domain.repository.UserTestRecordRepository;
 import com.dnd5.timoapi.domain.user.domain.entity.UserEntity;
 import com.dnd5.timoapi.domain.user.domain.repository.UserRepository;
 import com.dnd5.timoapi.domain.user.exception.UserErrorCode;
@@ -8,6 +11,7 @@ import com.dnd5.timoapi.domain.user.presentation.request.UpdateMeRequest;
 import com.dnd5.timoapi.domain.user.presentation.response.UserResponse;
 import com.dnd5.timoapi.global.exception.BusinessException;
 import com.dnd5.timoapi.global.security.context.SecurityUtil;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +22,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserTestRecordRepository userTestRecordRepository;
     private final RefreshTokenRepository refreshTokenRepository;
+
+    private final UserTestRecordService userTestRecordService;
 
     @Transactional(readOnly = true)
     public UserResponse getMe() {
@@ -31,9 +38,17 @@ public class UserService {
         user.update(request.name());
     }
 
-    @Transactional(readOnly = true)
+    @Transactional
     public void deleteMe() {
         UserEntity userEntity = getCurrentUserEntity();
+
+        List<UserTestRecordEntity> userTestRecordEntityList =
+                userTestRecordRepository.findByUserIdAndDeletedAtIsNull(userEntity.getId());
+
+        userTestRecordEntityList.forEach(
+                userTestRecordEntity -> userTestRecordService.delete(userTestRecordEntity.getId())
+        );
+
         userEntity.softDelete();
         refreshTokenRepository.deleteByUserId(userEntity.getId());
     }
