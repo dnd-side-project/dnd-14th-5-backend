d24c3e51c2308a34b2c7632e6d46037856e7e8cc
reyang <didfodms@gmail.com>
2026-04-02 21:04:34 +0900

    refactor: 사용하지 않는 import문 삭제

--- src/test/java/com/dnd5/timoapi/domain/reflection/application/support/UserTestRecordServiceTest.java
+++ src/test/java/com/dnd5/timoapi/domain/reflection/application/support/UserTestRecordServiceTest.java
@@ -2,8 +2,6 @@ package com.dnd5.timoapi.domain.reflection.application.support;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
