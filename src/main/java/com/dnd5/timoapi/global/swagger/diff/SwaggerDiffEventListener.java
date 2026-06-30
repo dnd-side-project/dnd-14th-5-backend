e6804ec4f755802a9d2d4e2bff1c7e1cd071f30f
reyang <didfodms@gmail.com>
2026-06-30 17:50:50 +0900

    chore: Swagger diff 이벤트 리스너 prod 환경에서만 실행되도록 제한

--- src/main/java/com/dnd5/timoapi/global/swagger/diff/SwaggerDiffEventListener.java
+++ src/main/java/com/dnd5/timoapi/global/swagger/diff/SwaggerDiffEventListener.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Slf4j
+@Profile("prod")
 @Component
 @RequiredArgsConstructor
 public class SwaggerDiffEventListener {
